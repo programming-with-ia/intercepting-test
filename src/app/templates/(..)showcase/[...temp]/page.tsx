048519f2418f6d800dd9f8d102e6ece98a30a3ae
immi <iafullprogrammer@gmail.com>
2024-11-10 21:55:00 +0500

    Update page.tsx

--- src/app/templates/(..)showcase/[...temp]/page.tsx
+++ src/app/templates/(..)showcase/[...temp]/page.tsx
@@ -5,7 +5,7 @@ async function Page({params}:{params: {temp: string[]}}) {
     const {temp} = await params
     console.log(temp)
   return (
-    <div className='text-lg text-red-400'>Showcase Simple Page
+    <div className='text-lg text-red-400'>Showcase Intercepting Page
         <Link href={"/templates/single"}>Single</Link>
     </div>
   )
