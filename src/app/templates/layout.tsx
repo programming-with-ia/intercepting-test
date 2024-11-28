3281de14c16452fc60823e82a0a2d24a07e47af0
Xavier Coppens <xavier@houd.art>
2024-11-28 08:04:27 +0000

    Added default.tsx and layout.tsx

---
+++ src/app/templates/layout.tsx
@@ -0,0 +1,14 @@
+export default function TemplatesLayout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
+  return (
+    <>
+      {modal}
+      {children}
+    </>
+  );
+}
